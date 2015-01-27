7f582ab150e822402b1722ff5d8351e08961d8e1
Johannes Boß <joha.boss@gmail.com>
2015-01-27 12:48:32 +0100

    Tried to do some tests

--- Ork.Energy.Tests/EnergyRepositoryTest.cs
+++ Ork.Energy.Tests/EnergyRepositoryTest.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region License
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//  
+// http://www.apache.org/licenses/LICENSE-2.0.html
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  
+// Copyright (c) 2015, HTW Berlin
+
+#endregion
+
 using System.ComponentModel.Composition;
 using NUnit.Framework;
 
 namespace Ork.Energy.Tests
 {
-    internal class EnergyRepositoryTest
-    {
-      private IEnergyRepository m_Repository;
+  internal class EnergyRepositoryTest
+  {
+    private readonly IEnergyRepository m_Repository;
 
-      [ImportingConstructor]
-      public EnergyRepositoryTest([Import] IEnergyRepository mConsumerRepository)
-      {
-        m_Repository = mConsumerRepository;
-      }
-      
-      [Test]
-      public void InitializeRepository()
-      {
-        Assert.NotNull(m_Repository);
-      }
+    [ImportingConstructor]
+    public EnergyRepositoryTest([Import] IEnergyRepository mConsumerRepository)
+    {
+      m_Repository = mConsumerRepository;
     }
+
+    [Test]
+    public void InitializeRepository()
+    {
+      Assert.NotNull(m_Repository);
+    }
+  }
 }
--- Ork.Energy.Tests/SearchFilterTest.cs
+++ Ork.Energy.Tests/SearchFilterTest.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿#region License
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//  
+// http://www.apache.org/licenses/LICENSE-2.0.html
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  
+// Copyright (c) 2015, HTW Berlin
+
+#endregion
+
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.Services.Client;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Moq;
+using NUnit.Framework;
+using Ork.Energy.DomainModelService;
+using Ork.Energy.Factories;
+using Ork.Energy.ViewModels;
+using Ork.Framework;
 
 namespace Ork.Energy.Tests
 {
-  class SearchFilterTest
+  [TestFixture]
+  internal class SearchFilterTest
   {
+    private EnergyManagementViewModel m_EnergyManagementViewModel;
+    private Mock<IDialogManager> m_DialogManagerMock;
+    private Mock<IEnergyViewModelFactory> m_EnergyViewModelFactoryMock;
+    private Mock<IEnergyRepository> m_Repository;
+    private Mock<DomainModelContext> m_Context;
+
+    [SetUp]
+    public void SetUp()
+    {
+      m_DialogManagerMock = new Mock<IDialogManager>();
+      m_EnergyViewModelFactoryMock = new Mock<IEnergyViewModelFactory>();
+      m_Repository = new Mock<IEnergyRepository>();
+
+
+      var items = new Consumer[]
+      {new Consumer(), new Consumer(), }.AsQueryable();
+
+      DataServiceCollection<Consumer> sdf = new DataServiceCollection<Consumer>();
+      sdf.Load(items);
+
+       m_Repository.SetupGet(r => r.Consumers).Returns(sdf);
+   
+      m_EnergyManagementViewModel = new EnergyManagementViewModel(m_Repository.Object, m_EnergyViewModelFactoryMock.Object, m_DialogManagerMock.Object);
+
+      m_EnergyManagementViewModel.ClickedConsumerGroup = new ConsumerGroupViewModel(new ConsumerGroup() {GroupName = "Consumer"}, m_Repository.Object);
+      m_EnergyManagementViewModel.ClickedDistributor = new DistributorViewModel(new Distributor(){Name = "Verteiler"}, m_Repository.Object);
+      m_EnergyManagementViewModel.NewConsumerName = "NeuerVerbraucher";
+ 
+      m_EnergyManagementViewModel.AddNewConsumer();
+
+      
+    }
+
+    [Test]
+    public void NoRestrictions()
+    {
+      Assert.IsTrue(m_EnergyManagementViewModel.Consumers.Count() == 1);
+    }
+
+
   }
 }
